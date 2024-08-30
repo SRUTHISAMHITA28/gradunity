b7a963462ef22bacfd7cbeab5c6659fcc20a778d
SRUTHISAMHITA28 <141323698+SRUTHISAMHITA28@users.noreply.github.com>
2024-08-30 07:05:31 +0530

    Create Mentorship.js

---
+++ Mentorship.js
@@ -0,0 +1,14 @@
+// src/components/Mentorship.js
+import React from 'react';
+
+function Mentorship() {
+  return (
+    <div className="mentorship">
+      <h2>Mentorship Program</h2>
+      <p>Become a mentor or find a mentor to guide your career.</p>
+      {/* Implement mentor/mentee profile creation and management */}
+    </div>
+  );
+}
+
+export default Mentorship;
