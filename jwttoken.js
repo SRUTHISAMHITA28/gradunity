ad92c10224bc80bf89be9c27c15fcc0d05652029
SRUTHISAMHITA28 <141323698+SRUTHISAMHITA28@users.noreply.github.com>
2024-08-30 11:15:25 +0530

    Create jwttoken.js

---
+++ jwttoken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateJWT = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Auth Error' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (e) {
+    return res.status(401).json({ message: 'Invalid Token' });
+  }
+};
