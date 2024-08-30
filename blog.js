b2cf51757f22f24adaed1396acef5fe54e6a12ea
SRUTHISAMHITA28 <141323698+SRUTHISAMHITA28@users.noreply.github.com>
2024-08-30 07:01:17 +0530

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,14 @@
+// src/components/Blog.js
+import React from 'react';
+
+function Blog() {
+  return (
+    <div className="blog">
+      <h2>Write a Blog</h2>
+      <p>Share your thoughts and experiences with the community.</p>
+      {/* Implement blog writing and submission features */}
+    </div>
+  );
+}
+
+export default Blog;
