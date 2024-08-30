eb7ba1f9d3870312ea51695cd26bc3ea8751534c
SRUTHISAMHITA28 <141323698+SRUTHISAMHITA28@users.noreply.github.com>
2024-08-30 07:07:24 +0530

    Create Survey.js

---
+++ Survey.js
@@ -0,0 +1,14 @@
+// src/components/Survey.js
+import React from 'react';
+
+function Survey() {
+  return (
+    <div className="survey">
+      <h2>Feedback Survey</h2>
+      <p>Your feedback helps us improve the platform.</p>
+      {/* Implement survey form and submission */}
+    </div>
+  );
+}
+
+export default Survey;
